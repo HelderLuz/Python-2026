ab5d4b8293bfb09119e256259dbca58a46c1f354
HelderLuz <67494949+HelderLuz@users.noreply.github.com>
2026-08-11 20:13:35 -0300

    A12 - ex3

---
+++ Aula 12/Exercícios/ex2.py
@@ -0,0 +1,6 @@
+# 2. Utilize a depuração para identificar e corrigir o erro do seguinte código:
+
+x = 10
+while x >= 0:
+    print(x)
+    x -= 1  
---
+++ Aula 12/Exercícios/ex3.py
@@ -0,0 +1,9 @@
+num = 1
+soma_impares = 0
+
+while num <= 10:
+    if num % 2 != 0:
+        soma_impares = soma_impares + num
+    num = num + 1
+
+# 3. Faça o teste de mesa e use o depurador para analisar como a variável soma_impares é atualizada ao longo do laço. Identifique o valor final da variável.
