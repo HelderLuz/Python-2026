5fd9b40f10e205b513a1ab8aa0ac7a057fc61e1d
HelderLuz <67494949+HelderLuz@users.noreply.github.com>
2026-08-11 21:29:41 -0300

    A12 - ex5

---
+++ Aula 12/Exercícios/ex5.py
@@ -0,0 +1,8 @@
+a = 1
+produto = 1
+
+while a <= 5:
+    produto *= a
+    a += 1
+
+# 5. Usando o teste de mesa, determine o valor final de produto. Depois, use o depurador para verificar a execução linha por linha e comparar os resultados.
